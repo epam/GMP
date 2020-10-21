c81434e57f913e0edbb04c1f038acaf3c7fe90d8
asamuta <a.samuta@gmail.com>
2020-10-21 18:51:04 +0300

    Code refactoring

--- src/test/resources/gmp-home/scripts/test_03-executor/exec-00.groovy
+++ src/test/resources/gmp-home/scripts/test_03-executor/exec-00.groovy
@@ -1,12 +1,12 @@
 import com.epam.dep.esp.common.OS
 import com.epam.gmp.ScriptResult
 
-
-def os = OS.getOs()
-def out = new ArrayList()
+def os = OS.os
+def out = []
 def result
-if (os == OS.win)
+if (os == OS.win) {
     result = os.execCommandLine(['cmd.exe', '/c', 'dir'], out, '.', 30)
+}
 logger.info "$result"
 logger.info out.join("\n\r")
 Thread.currentThread().sleep(1000)
